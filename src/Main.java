2afad5329eea1e8293ff8ff07ec4a0b9e3eac8a2
Nicolo' Ferri <irr3f92@gmail.com>
2023-06-27 20:21:40 +0200

    Ho aggiuto menuPrimi

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Map<String, Piatto> menuPrimi = new HashMap<String, Piatto>();
+        menuPrimi.put("Spaghetti alla carbonara", new Piatto(10, "Pasta condita con uova, pancetta, pecorino e pepe"));
+        menuPrimi.put("Lasagne al Ragù", new Piatto(15, "Strati di pasta, ragù di carne e besciamella"));
+        menuPrimi.put("Penne all'Arrabbiata", new Piatto(11, "Pasta condita con salsa di pomodoro, peperoncino e aglio"));
+        menuPrimi.put("Risotto ai Funghi", new Piatto(15, "Riso cotto lentamente con funghi porcini e brodo"));
+        menuPrimi.put("Gnocchi al Pesto", new Piatto(8, "Gnocchi di patate conditi con pesto alla genovese"));
 
+        Piatto.printMenu(menuPrimi);
     }
 }
