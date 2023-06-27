a2fd489034e3ddbec7ac81b79b2155519a1cd7c1
FrancescoSko <francescobuonora11@gmail.com>
2023-06-27 21:24:13 +0200

    Aggiunto menu Secondi

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,25 @@ public class Main {
         menuPrimi.put("Gnocchi al Pesto", new Piatto(8, "Gnocchi di patate conditi con pesto alla genovese"));
 
         Piatto.printMenu(menuPrimi);
+   
+        Map<String, Piatto> menuSecondi = new HashMap<String, Piatto>();
+        menuSecondi.put("Scaloppine al limone", new Piatto(12, "Fettine di vitello  battute e poi cotte in padella \ncon succo di limone, burro e prezzemolo"));
+        menuSecondi.put("Arrosto di maiale", new Piatto(11, "Arrosto di delizioso filetto di maialino nero"));
+        menuSecondi.put("Bistecca alla fiorentina", new Piatto(24, "Bistecca di carne marchigiana con taglio da fiorentina"));
+        menuSecondi.put("Abbacchio alla romana", new Piatto(16, "Carne di agnello da latte marinata con aglio, rosmarino, pepe nero\n e succo di limone cotta lentamente in padella"));
+        menuSecondi.put("Pollo alla cacciatora", new Piatto(14, "Pollo allevato a terra \ncon  contorno di patate al forno"));
+
+
+
+
+        Piatto.printMenu(menuSecondi);
+   
+   
+   
+   
+   
+   
+   
+   
     }
 }
