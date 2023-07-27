2569031b4a997ddbe6f2d97f44eaafb0c9a13969
Nicolo' Ferri <irr3f92@gmail.com>
2023-07-27 22:25:28 +0200

    conflitti risolti nella classa Bevanda

--- src/Bevanda.java
+++ src/Bevanda.java
@@ -1,26 +1,12 @@
 import java.util.List;
 
-<<<<<<< HEAD
-public class Bevanda extends Portata {
-
-    //TODO che la facciamo a fare è uguale a portata? le classi figlio devono estendere comportamenti e avere altri field: grado alcolico
-
-    public Bevanda(String name, Double price, String description, List<AllergeniEnum> allergeni) {
-=======
 public class Bevanda extends Portata{
     private Double tassoAlcolemico;
 
-
-    //TODO che la facciamo a fare è uguale a portata? le classi figlio devono estendere comportamenti e avere altri field: grado alcolico
-
     public Bevanda(String name, Double price, String description, Double tassoAlcolemico, List<String> allergeni) {
->>>>>>> 38ce93793fc635c6e908f96c6d19a7bb11f7886c
         super(name, price, description, allergeni);
         this.tassoAlcolemico = tassoAlcolemico;
     }
-
-<<<<<<< HEAD
-=======
 
     public Double getTassoAlcolemico() {
         return tassoAlcolemico;
@@ -29,5 +15,5 @@ public class Bevanda extends Portata{
     public void setTassoAlcolemico(Double tassoAlcolemico) {
         this.tassoAlcolemico = tassoAlcolemico;
     }
->>>>>>> 38ce93793fc635c6e908f96c6d19a7bb11f7886c
+
 }
